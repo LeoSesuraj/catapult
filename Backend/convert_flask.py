171d64b82d4b2314246366775be9b639f498aed1
Velan Arunmozhi <varunmoz@purdue.edu>
2025-04-13 06:13:30 -0400

    flask

--- Backend/convert_flask.py
+++ Backend/convert_flask.py
@@ -1,5 +1,8 @@
 import travel_agents
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/createitinerary", methods=["POST", "GET"])
 def convert_flask():
     return travel_agents.pipeline("IND", "JFK", "04/13/2025","04/16/2025")
-
